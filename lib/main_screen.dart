22b561b6116516352befe416ba9b7184b18b59cf
Krass96 <gyncoo@abv.bg>
2025-05-23 13:37:35 +0200

    Refactor MainScreen to StatefulWidget and implement length tracking (Bonus)

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,8 +1,24 @@
 import 'package:callback_functions/input_counter_box.dart';
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+class MainScreen extends StatefulWidget {
+  final void Function(int)? summeofLengths;
+
+  const MainScreen({super.key, this.summeofLengths});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _length = 0;
+
+  void _incrementLength() {
+    setState(() {
+      _length++;
+    });
+    widget.summeofLengths?.call(_length);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,20 +32,24 @@ class MainScreen extends StatelessWidget {
               color: Colors.amber,
               onLengthChanged: (text) {
                 debugPrint('Box 1 wurde verändert');
+                _incrementLength();
               },
             ),
             InputCounterBox(
               color: Colors.greenAccent,
               onLengthChanged: (text) {
                 debugPrint('Box 2 wurde verändert');
+                _incrementLength();
               },
             ),
             InputCounterBox(
               color: Colors.deepOrangeAccent,
               onLengthChanged: (text) {
                 debugPrint('Box 3 wurde verändert');
+                _incrementLength();
               },
             ),
+            Text('Summe aller Längen: $_length'),
           ],
         ),
       ),
